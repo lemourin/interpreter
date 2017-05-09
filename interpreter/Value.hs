8368f7bd845b544896d3123dd670316d2efa6aa0
Paweł Wegner <pawel.wegner95@gmail.com>
2017-05-09 11:17:15 +0200

    Fixed Value.hs.

--- interpreter/Value.hs
+++ interpreter/Value.hs
@@ -23,37 +23,48 @@ show' ValueVoid = ""
 add' (ValueInteger v1) (ValueInteger v2) = Ok (ValueInteger (v1 + v2))
 add' (ValueString v1) (ValueString v2) = Ok (ValueString (v1 ++ v2))
 add' _ _ = Bad "add' :: Invalid types"
+
 sub' (ValueInteger v1) (ValueInteger v2) = Ok (ValueInteger (v1 - v2))
 sub' _ _ = Bad "sub' :: Invalid types"
+
 mul' (ValueInteger v1) (ValueInteger v2) = Ok (ValueInteger (v1 * v2))
 mul' _ _ = Bad "mul' :: Invalid types"
+
 div' (ValueInteger v1) (ValueInteger v2) = Ok (ValueInteger (v1 `div` v2))
 div' _ _ = Bad "div' :: Invalid types"
+
 equal' (ValueInteger v1) (ValueInteger v2) = Ok (ValueBool (v1 == v2))
 equal' (ValueString v1) (ValueString v2) = Ok (ValueBool (v1 == v2))
 equal' (ValueBool v1) (ValueBool v2) = Ok (ValueBool (v1 == v2))
 equal' _ _ = Bad "equal' :: Invalid types"
+
 nequal' (ValueInteger v1) (ValueInteger v2) = Ok (ValueBool (v1 /= v2))
 nequal' (ValueString v1) (ValueString v2) = Ok (ValueBool (v1 /= v2))
 nequal' (ValueBool v1) (ValueBool v2) = Ok (ValueBool (v1 /= v2))
 nequal' _ _ = Bad "nequal' :: Invalid types"
+
 gequal' (ValueInteger v1) (ValueInteger v2) = Ok (ValueBool (v1 >= v2))
 gequal' (ValueString v1) (ValueString v2) = Ok (ValueBool (v1 >= v2))
 gequal' (ValueBool v1) (ValueBool v2) = Ok (ValueBool (v1 >= v2))
 gequal' _ _ = Bad "gequal' :: Invalid types"
+
 lequal' (ValueInteger v1) (ValueInteger v2) = Ok (ValueBool (v1 <= v2))
 lequal' (ValueString v1) (ValueString v2) = Ok (ValueBool (v1 <= v2))
 lequal' (ValueBool v1) (ValueBool v2) = Ok (ValueBool (v1 <= v2))
 lequal' _ _ = Bad "lequal' :: Invalid types"
+
 greater' (ValueInteger v1) (ValueInteger v2) = Ok (ValueBool (v1 > v2))
 greater' (ValueString v1) (ValueString v2) = Ok (ValueBool (v1 > v2))
 greater' (ValueBool v1) (ValueBool v2) = Ok (ValueBool (v1 > v2))
 greater' _ _ = Bad "greater' :: Invalid types"
+
 less' (ValueInteger v1) (ValueInteger v2) = Ok (ValueBool (v1 < v2))
 less' (ValueString v1) (ValueString v2) = Ok (ValueBool (v1 < v2))
 less' (ValueBool v1) (ValueBool v2) = Ok (ValueBool (v1 < v2))
 less' _ _ = Bad "less' :: Invalid types"
+
 or' (ValueBool v1) (ValueBool v2) = Ok (ValueBool (v1 || v2))
 or' _ _ = Bad "or' :: Invalid types"
+
 and' (ValueBool v1) (ValueBool v2) = Ok (ValueBool (v1 && v2))
 and' _ _ = Bad "and' :: Invalid types"
